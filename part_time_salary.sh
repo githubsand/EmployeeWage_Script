01cfd8417e1437f7bb161ef85ad6148ea7e00dbe
FamilyMan <40295924+githubsand@users.noreply.github.com>
2020-12-13 13:14:56 +0530

    Update part_time_salary.sh

--- part_time_salary.sh
+++ part_time_salary.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 wage_per_hour=20
 part_time=4
-salary=$(($wage_per_hour*$part_time))
+ispresent=1
+check=$((RANDOM%2))
+if [ $check -eq $ispresent ]
+then
+    salary=$(($wage_per_hour*$part_time))
+else
+    salary=0
+fi   
 echo "Part Time Salary is:" $salary
 
 
