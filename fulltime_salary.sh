5520ccbc2137f8c45186bbf754951cb39018e2d1
FamilyMan <40295924+githubsand@users.noreply.github.com>
2020-12-13 10:57:03 +0530

    Update fulltime_salary.sh

--- fulltime_salary.sh
+++ fulltime_salary.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 wage_per_hour=20
 full_day=8
-salary=$(($wage_per_hour*$full_day))
+ispresent=1
+check=$((RANDOM%2))
+if [ $check -eq $ispresent ]
+then
+    salary=$(($wage_per_hour*$full_day))
+else
+   salary=0
+fi   
 echo "Full Time Employee salary is :" $salary
 
 
